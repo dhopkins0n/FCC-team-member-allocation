1394b4db02262a038244559d17eaaa1b796d0754
David Hopkinson <167008664+dhopkins0n@users.noreply.github.com>
2024-08-22 20:33:06 +0000

    changing vite again

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: 'https://dhopkins0n.github.io/FCC-team-member-allocation/',
+  base: '/FCC-team-member-allocation/',
   plugins: [react()],
 });
