0a76506caeae7ea7eba1245d47dab0d6e5350c36
David Hopkinson <167008664+dhopkins0n@users.noreply.github.com>
2024-08-22 17:10:28 +0000

    changing base in vite to try and host

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/FCC-team-member-allocation/',
+  base: 'https://dhopkins0n.github.io/FCC-team-member-allocation/',
   plugins: [react()],
 });
