04c3b77925cacd1e464e4defd5b82f1e7767d3bb
David Hopkinson <167008664+dhopkins0n@users.noreply.github.com>
2024-08-22 16:01:58 +0000

    updating vite etc to host

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/FCC-team-member-allocation/',
   plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-  }
-})
+});
