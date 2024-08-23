7708c4cf1a31007f7f238d58dad42979dc46f533
David Hopkinson <167008664+dhopkins0n@users.noreply.github.com>
2024-08-23 18:01:07 +0000

    commenting out base

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/FCC-team-member-allocation/', // Ensure the correct base path for GitHub Pages
+  // base: '/FCC-team-member-allocation/', // Ensure the correct base path for GitHub Pages
   plugins: [react()],
   build: {
     outDir: 'dist', // This is the default directory where Vite outputs the build files
